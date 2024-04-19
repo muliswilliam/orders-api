dce1cc3cf223a4ceaf92b7fda9d516cfad75d05f
William Muli <willi.wambu@gmail.com>
2024-04-19 09:35:50 +0300

    Refactored app

--- main.go
+++ main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/muliswilliam/orders-api/app"
 )
 
 func main () {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := app.New()
 
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("failed to start app:", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world"))
 }
